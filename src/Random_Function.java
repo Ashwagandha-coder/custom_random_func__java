51a66c61ff5e27a30c72c8195913289b39099c11
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-28 18:38:57 +0500

    done

--- src/Random_Function.java
+++ src/Random_Function.java
@@ -1,5 +1,26 @@
-public class Main {
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class Random_Function {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        PrintStream printStream = new PrintStream(System.out,true, StandardCharsets.UTF_8);
+        printStream.println("Введите диапазон min max через пробел");
+
+        System.out.println(new Random_Function().rnd(scanner.nextInt(), scanner.nextInt()));
+    }
+
+    public int rnd(int min, int max) {
+
+        max -= min;
+
+        return (int) (Math.random() * ++max) + min;
+
+
     }
 }
